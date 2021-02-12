5fd299a67f1cdd075532302fd01433fa0c02ca90
matricola787 <62719360+matricola787@users.noreply.github.com>
2021-02-12 16:49:14 +0100

    Update cyclic_animation_interpolation.py

--- cyclic_animation_interpolation/cyclic_animation_interpolation.py
+++ cyclic_animation_interpolation/cyclic_animation_interpolation.py
@@ -24,7 +24,7 @@ bl_info = {
     "name": "Cyclic animation interpolation",
     "description": "Interpolates the entire animation cycle taking the extremes into account",
     "author": "matricola787",
-    "version": (1, 0),
+    "version": (1, 1),
     "blender": (2, 90, 0),
     "location": "Key > Interpolate Cycle",
     "warning": "",
